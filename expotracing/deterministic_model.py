06eb7beee62e4bf3962fe2ac66cafadd865096d4
angieburdinski <aburdinski@web.de>
2021-01-26 14:41:53 +0100

    ..

--- expotracing/deterministic_model.py
+++ expotracing/deterministic_model.py
@@ -3,7 +3,8 @@ Deterministic Models for Digital Exposure Tracing
 """
 import epipack
 import numpy as np
-import matplotlib.pyplot as plt
+from bfmplot import pl as plt
+
 class ParamDict(dict):
     def __init__(self, *args, **kwargs):
         dict.__init__(self, *args, **kwargs)
@@ -11,60 +12,28 @@ class ParamDict(dict):
 
 class BaseModel():
     """Defines the BaseModel"""
-    integrator = 'dopri5'
-    def __init__(self,epimodel,t0,):
-        self.epimodel = epimodel
+    def __init__(self,model,t0):
+        self.model = model
         self.t0 = t0
 
-    def set_parameters(self,parameters):
+    def compute(self, t):
         """
-        This function takes a parameter dictionary
-        and updates the numerical ODEs accordingly.
-        This method is supposed to be overridden by
-        child classes.
-        """
-        pass
-
-    def compute(self, time_points=None):
-        """
-        This function carries out the model integration
-        given the parameters that have been set.
-        Parameters
-        ----------
-        time_points : list or numpy.ndarray of float, default: None
-            An ordered iterable that contains
-            the time points at which the model
-            should be evaluated
+        This function integrates the model over time t.
+        t has to be list or numpy.ndarray of float.
         Returns
         -------
         result : dict
             The model result in the following format
             .. code:: python
             {
-                'compartments' : {
                     'S' : [ 10000, 99999, 99999.5, ...],
                     'I' : [ 1, 2, 2.5, ...],
                     ...
                 }
-            }
-
         """
-
-        if time_points is None:
-            if self.time_points is not None:
-                time_points = self.time_points
-            else:
-                raise ValueError('integration time points are not set')
-
-        result = self.epimodel.integrate(time_points, integrator=self.integrator)
-        result = {
-                    'compartments': result
-                 }
-
+        result = self.model.integrate(t, integrator='dopri5')
         self.result = result
-
         return result
-
 
 class SIRX(BaseModel):
     """
@@ -73,47 +42,42 @@ class SIRX(BaseModel):
     epidemic SIR system.
     """
 
-    def __init__(self,population_size, t0):
+    def __init__(self, N, t0):
 
-        self.population_size = population_size
-
-        model = epipack.EpiModel(['S','I','R','X'],initial_population_size=population_size)
-
+        self.N = N
+        model = epipack.EpiModel(['S','I','R','X'],initial_population_size = N)
         BaseModel.__init__(self, model, t0)
 
     def set_parameters(self,parameters):
         """
-        Set the parameter values for this model
+        Set the parameter values for the model
         Parameters
         ----------
         parameters : dict
-
                 {
                     'R0' : ...,
                 }
         """
         p = ParamDict(parameters)
         kappa = (p.q*p.rho)/(1-p.q)
-        self.epimodel.set_processes([
+        self.model.set_processes([
                     ('I', p.rho, 'R' ),
                     ('I', kappa, 'X' ),
                     ('I', 'S', p.R0*p.rho, 'I', 'I' ),
                     ('I', 'I', kappa * p.a**2 * p.k0, 'I', 'X' )]),
+        self.model.set_initial_conditions({'I': p.I_0, 'S': self.N - p.I_0})
 
-        self.epimodel.set_initial_conditions({'I': p.I_0, 'S': self.population_size - p.I_0})
 class SIRXQ(BaseModel):
     """
-    This class describes a first generation tracing system without
+    This class describes a first generation tracing system with
     quarantining of susceptible contacts. It is based on the basic
     epidemic SIR system.
     """
 
-    def __init__(self,population_size, t0):
+    def __init__(self, N, t0):
 
-        self.population_size = population_size
-
-        model = epipack.EpiModel(['S','I','R','X','Q'],initial_population_size=population_size)
-
+        self.N = N
+        model = epipack.EpiModel(['S','I','R','X','Q'],initial_population_size = N)
         BaseModel.__init__(self, model, t0)
 
     def set_parameters(self,parameters):
@@ -122,36 +86,32 @@ class SIRXQ(BaseModel):
         Parameters
         ----------
         parameters : dict
-
                 {
                     'R0' : ...,
                 }
         """
         p = ParamDict(parameters)
         kappa = (p.q*p.rho)/(1-p.q)
-        self.epimodel.set_processes([
+        self.model.set_processes([
                     ('I', p.rho, 'R' ),
                     ('I', kappa, 'X' ),
                     ('Q', p.omega, 'S' ),
                     ('I', 'S', p.R0*p.rho, 'I', 'I' ),
                     ('I', 'S', kappa * p.a**2 * p.k0, 'I', 'Q' ),
                     ('I', 'I', kappa * p.a**2 * p.k0, 'I', 'X' )])
-
-        self.epimodel.set_initial_conditions({'I': p.I_0, 'S': self.population_size - p.I_0})
+        self.model.set_initial_conditions({'I': p.I_0, 'S': self.N - p.I_0})
 
 class SIRTX(BaseModel):
     """
-    This class describes a first generation tracing system without
+    This class describes a next generation tracing system without
     quarantining of susceptible contacts. It is based on the basic
     epidemic SIR system.
     """
 
-    def __init__(self,population_size, t0):
+    def __init__(self, N, t0):
 
-        self.population_size = population_size
-
-        model = epipack.EpiModel(['S','I','R','T','X'],initial_population_size=population_size)
-
+        self.N = N
+        model = epipack.EpiModel(['S','I','R','T','X'],initial_population_size = N)
         BaseModel.__init__(self, model, t0)
 
     def set_parameters(self,parameters):
@@ -160,35 +120,31 @@ class SIRTX(BaseModel):
         Parameters
         ----------
         parameters : dict
-
                 {
                     'R0' : ...,
                 }
         """
         p = ParamDict(parameters)
         kappa = (p.q*p.rho)/(1-p.q)
-        self.epimodel.set_processes([
+        self.model.set_processes([
                             ('I', p.rho, 'R'),
                             ('I', kappa, 'T' ),
                             ('T', p.chi, 'X' ),
                             ('I', 'S', p.R0*p.rho, 'I', 'I'),
                             ('T', 'I', p.chi * p.a**2 * p.k0, 'T', 'T' )])
-
-        self.epimodel.set_initial_conditions({'I': p.I_0, 'S': self.population_size - p.I_0})
+        self.model.set_initial_conditions({'I': p.I_0, 'S': self.N - p.I_0})
 
 class SIRTXQ(BaseModel):
     """
-    This class describes a first generation tracing system without
+    This class describes a next generation tracing system with
     quarantining of susceptible contacts. It is based on the basic
     epidemic SIR system.
     """
 
-    def __init__(self,population_size, t0):
+    def __init__(self, N, t0):
 
-        self.population_size = population_size
-
-        model = epipack.EpiModel(['S','I','R','T','X','Q'],initial_population_size=population_size)
-
+        self.N = N
+        model = epipack.EpiModel(['S','I','R','T','X','Q'],initial_population_size=N)
         BaseModel.__init__(self, model, t0)
 
     def set_parameters(self,parameters):
@@ -197,14 +153,13 @@ class SIRTXQ(BaseModel):
         Parameters
         ----------
         parameters : dict
-
                 {
                     'R0' : ...,
                 }
         """
         p = ParamDict(parameters)
         kappa = (p.q*p.rho)/(1-p.q)
-        self.epimodel.set_processes([
+        self.model.set_processes([
                     ('I', 'S', p.R0/p.rho, 'I', 'I' ),
                     ('T', 'S', p.chi * p.a**2 * p.k0, 'T', 'Q' ),
                     ('T', 'I', p.chi * p.a**2 * p.k0, 'T', 'T' ),
@@ -213,13 +168,14 @@ class SIRTXQ(BaseModel):
                     ('T', p.chi, 'X'),
                     ('Q', p.omega, 'S')])
 
-        self.epimodel.set_initial_conditions({'I': p.I_0, 'S': self.population_size - p.I_0})
+        self.model.set_initial_conditions({'I': p.I_0, 'S': self.N - p.I_0})
+
 
 if __name__=="__main__":
-    N = 1000
+    population_size = 1000
     t = np.linspace(0,300,1000)
-    model = SIRX(population_size = N, t0 = 0)
-    P = {
+    model = SIRX(N = population_size, t0 = 0)
+    parameter = {
             'R0': 2.5,
             'q': 0.3,
             'a': 0.5,
@@ -227,12 +183,12 @@ if __name__=="__main__":
             'k0' : 50,
             'I_0' : 1
             }
-    model.set_parameters(P)
+    model.set_parameters(parameter)
     result = model.compute(t)
-    plt.plot(t,result['compartments']['S'],label = 'S')
-    plt.plot(t,result['compartments']['I'],label = 'I')
-    plt.plot(t,result['compartments']['R'],label = 'R')
-    plt.plot(t,result['compartments']['X'],label = 'X')
+    plt.plot(t,result['S'],label = 'S')
+    plt.plot(t,result['I'],label = 'I')
+    plt.plot(t,result['R'],label = 'R')
+    plt.plot(t,result['X'],label = 'X')
     plt.legend()
     plt.xlabel('time [d]')
     plt.ylabel('individuals')
