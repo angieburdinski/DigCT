73048b9b4e66194681d56d124fbb8bcd903b0af1
angieburdinski <aburdinski@web.de>
2021-02-02 10:35:56 +0100

    ..

--- expotracing/tests.py
+++ expotracing/tests.py
@@ -1,5 +1,5 @@
 from extendedmodel import mixed_tracing
-from tools import analysis 
+from tools import analysis
 from plots import plot
 from matplotlib import colors as mcolors
 from matplotlib.lines import Line2D
@@ -8,7 +8,7 @@ import numpy as np
 color =  dict(mcolors.TABLEAU_COLORS, **mcolors.CSS4_COLORS)
 colors = [i for i in color.keys()]
 N = 1000
-t = np.linspace(0,1000,4000)
+t = np.linspace(0,1000,1000)
 
 model = mixed_tracing(N,quarantine_S_contacts = True)
 parameter = {
@@ -23,20 +23,27 @@ parameter = {
         'x':0.17,
         'y':0.1,
         'z':0.64,
-        'I_0' : 1,
+        'I_0' : 10,
         'omega':1/10
         }
+from matplotlib import rc
+plt.rcParams.update({'font.size': 13})
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+q = [0,0.2,0.4,0.6,0.8]
 
-q = [0,0.01,0.2,0.4,0.6,0.8]
-#number_of_contacts = np.linspace(0,100,50)
-a = np.linspace(0,1,10)
+a = np.linspace(0,1,50)
 result = analysis(model,parameter,t).two_range_result('app_participation',a,'q',q)
+fig, axs = plt.subplots(1,2,figsize = (12,3))
 for i in a:
     for j in q:
-        plt.plot(i, result[i][j]['R'].max(axis = 0)+result[i][j]['X'].max(axis = 0),'.', color = colors[q.index(j)])
+        axs[0].plot(i, (result[i][j]['R'].max(axis = 0)+result[i][j]['X'].max(axis = 0))/N,'.', color = colors[q.index(j)])
+        axs[1].plot(i, (result[i][j]['I_S'].max(axis = 0))/N,'.', color = colors[q.index(j)])
 lines = [Line2D([0], [0], color=colors[x], linewidth=3, linestyle='dotted') for x in range(len(q))]
-labels = [('q =' + str(j)) for j in q]
-plt.legend(lines, labels)
-plt.xlabel('app participation')
-plt.ylabel('individuals')
+labels = [('q = ' + str(j)) for j in q]
+axs[0].legend(lines, labels)
+axs[1].legend(lines, labels)
+axs[0].set_xlabel('app participation')
+axs[1].set_xlabel('app participation')
+axs[0].set_ylabel(r'$R (t\rightarrow \infty)+X (t\rightarrow \infty)$')
+axs[1].set_ylabel(r'$I_{S_{max}}$')
 plt.show()
